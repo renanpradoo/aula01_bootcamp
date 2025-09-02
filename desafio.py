f566ddb3b8c75811aec87aeca782bd6cf5eb3e25
Renan Prado <renanpradoo@gmail.com>
2025-09-02 14:13:21 -0300

    ;

---
+++ desafio.py
@@ -0,0 +1,11 @@
+CTE = 1000
+
+name = input("Digite o nome: ")
+
+salario = float(input("Digite o salario: "))
+
+bonus = float(input("Digite o bonus: "))
+
+resultado = CTE + salario * bonus
+
+print(f"{name}, seu bonus e de: {resultado}")
